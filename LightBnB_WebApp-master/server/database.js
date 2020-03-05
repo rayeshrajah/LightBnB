9bfcecb6d94d26b1d3f90c2f17ce319599a9de65
Rayesh <rayeshrajah@gmail.com>
2020-03-05 14:57:43 +0000

    added getting user by email

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb"
+});
+
 /// Users
 
 /**
@@ -9,16 +17,13 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  let values = [email];
+  let queryString = `SELECT * 
+                     FROM users
+                     WHERE email = $1;`;
+ return pool
+  .query(queryString, values)
+  .then(res => console.log(res.rows))
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -28,7 +33,12 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  let values = [id];
+  let queryString = `SELECT * FROM users
+                     WHERE id = $1;`;
+  return pool 
+         .query(queryString, values)
+         .then(res => res.rows[0])
 }
 exports.getUserWithId = getUserWithId;
 
@@ -67,12 +77,15 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+  const limiter = limit;
+  const values = [limiter];
+  const queryString = `SELECT * 
+                       FROM properties
+                       LIMIT $1`;
+   return pool
+  .query(queryString, values)
+  .then(res => res.rows)
+};
 exports.getAllProperties = getAllProperties;
 
 
